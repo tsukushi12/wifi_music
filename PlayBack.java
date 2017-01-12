3444996ab44a2c463191b42dd39d0997bda56d0e
yuki.m <kd1244161@st.kobedenshi.ac.jp>
2017-01-12 14:49:57 +0900

    add PlayBack

--- PlayBack.java
+++ PlayBack.java
@@ -1,31 +1,23 @@
 import javax.sound.sampled.*;
 import java.io.*;
-public class sound {
+public class PlayBack {
+    final byte frame[] = new byte[1600];
+    AudioInputStream stream;
+    AudioFormat format;
+    DataLine.Info info;
+    SourceDataLine line;
     public static void main(String args[]){
-        try{
-           File audioFile = new File("musicbox.wav");
-           AudioInputStream stream = AudioSystem.getAudioInputStream(audioFile);
-           byte buffer[] = new byte[1600];
-           AudioFormat format = stream.getFormat();
-           
-           System.out.println(format.getProperty());
-//ソースデータライン取得
-           DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-           SourceDataLine line = (SourceDataLine)AudioSystem.getLine(info);
-//ソースデータラインをオープン
-           line.open(format);
-           line.start();
-           while(true)
-           {
-               if(stream.read(buffer, 0, buffer.length) == -1)
-                    break;
-               line.write(buffer, 0, buffer.length);
-           }
-           line.drain();
-           line.stop();
-           line.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+
+    }
+    PlayBack(File audiofile){
+         stream = new AudioInputAtream(audiofile);
+         format = stream.getFormat();
+         info = new DataLine.Info();
+    }
+    public int getDataSize(){
+        format.getProperty();
+    }
+    public void play(){
+
     }
 }
