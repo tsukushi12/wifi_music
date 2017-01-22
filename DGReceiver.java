88222faa48c06aff3f1b8c7edd5d43ec16656cd7
tsukushi12 <tsukushi129@gmail.com>
2017-01-23 03:11:20 +0900

    DG update

--- DGReceiver.java
+++ DGReceiver.java
@@ -1,13 +1,25 @@
 import java.io.*;
-import packet.DGSocket;
+import java.net.*;
 public class DGReceiver{
     public String bindIp;
     public int port = 10100;
-    public 
+    public DatagramSocket sock;
     public static void main(String args[]){
-
+        DGReceiver receiver = new DGReceiver();
+        
     }
     DGReceiver(){
-
+        try{
+            sock = new DatagramSocket(port);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public String recv(){
+        byte[] buf = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        sock.receive(packet);
+        String message = new String(buf, 0, packet.getLength());
+        return message;
     }
 }
--- DGSender.java
+++ DGSender.java
@@ -1,19 +1,45 @@
 import java.io.*;
-import packet.DGSocket;
+import java.net.*;
 public class DGSender{
     public String ip;
     public int port = 10100;
-    public DGSocket sock;
+    public DatagramSocket sock;
+    public String meta;
+    public DatagramPacket packet;
+    public InetSocketAddress dest;
     public static void main(String args[]){
-
+        
+    }
+    DGSender(){
+        getSocket();
+        dest = new InetSocketAddress("ff02::1", port);
     }
     DGSender(String ipaddress){
         ip = ipaddress;
-        sock = new DGSocket(ip, port);
+        dest = new InetSocketAddress(ip, port);
+        getSocket();
     }
-    public void addMeta(){
-
+    public void addMeta(String str){
+        meta = str;
+        byte buf[] = meta.getBytes();
+        packet = new DatagramPacket(buf, 0, buf.length, dest);
     }
     public void sendMetaAudio(){
+        try{
+            sock.send(packet);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void send(String str){
+        addMeta(str);
+        sendMetaAudio();
+    }
+    public void getSocket(){
+        try{
+            sock = new DatagramSocket();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
